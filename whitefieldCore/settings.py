0c0d81a659763ab9eedb16b06969736c535fe5b2
unknown <yuvrajl.103@gmail.com>
2022-03-08 14:48:10 +0530

    requirements txt

--- whitefieldCore/settings.py
+++ whitefieldCore/settings.py
@@ -33,7 +33,6 @@ ALLOWED_HOSTS = ["127.0.0.1","localhost","whitefieldfarm.herokuapp.com","3.111.1
 # Application definition
 
 INSTALLED_APPS = [
-    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
